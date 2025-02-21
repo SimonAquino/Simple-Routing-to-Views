dcaa786ae05283077677b9b7724eaa2a59e74988
SimonAquino <144974424+SimonAquino@users.noreply.github.com>
2025-02-21 08:17:55 +0000

    forgot to push...

---
+++ Simple-Routing-to-Views/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return 'YOOOOO WASSSUPPPPP!';
+});
+
+Route::get('/greet', [App\Http\Controllers\GreetController::class, 'show']);
